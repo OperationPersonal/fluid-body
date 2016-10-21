91ba89d15ffcd0620cc8f81ef9594decaae34393
Leon Chou <bleepbloopsify@gmail.com>
2016-10-21 00:04:53 -0400

    Attempted to boilerplate. Lesson tbd

---
+++ lib/gameruntime.py
@@ -0,0 +1,23 @@
+from dis import dis
+from pprint import pprint
+
+import pygame
+
+
+class GameRunTime(object):
+
+  def __init__(self):
+    pygame.init()
+    self._clock = pygame.time.Clock()
+    self._display_info = pygame.display.Info()
+    self._screen = pygame.display.set_mode(
+        (self._display_info.current_w >> 1, self._display_info.current_h >> 1), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE, 32)
+
+  def run(self):
+    pprint('hi')
+    pprint(vars(pygame.display))
+    pprint(dir(self._screen), indent=2)
+
+if __name__ == '__main__':
+  game = GameRunTime()
+  game.run()
---
+++ lib/lesson.py
@@ -0,0 +1,3 @@
+from pykinect2 import PyKinectRuntime as prun
+from pykinect2 import PyKinectV2 as pk
+from pykinect2.PyKinectV2 import *
