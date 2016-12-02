917fb72fb59ed3e865612861f525f5b5ae591fe9
Leon Chou <bleepbloopsify@gmail.com>
2016-12-02 12:48:05 -0500

    text to speech improvements

--- lib/audio.py
+++ lib/audio.py
@@ -4,10 +4,8 @@ class AudioInterface(object):
     def __init__(self):
         self._engine = pyttsx.init()
 
-    def output(self, text):
+    def queue(self, text):
         self._engine.say(text)
-        self._engine.runAndWait()
 
-if __name__ == '__main__':
-    audio = AudioInterface()
-    audio.output('hello')
+    def speak(self):
+        self._engine.runAndWait()
