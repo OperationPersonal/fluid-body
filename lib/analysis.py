a69835ff329773e063f44ca43234082186fc3cc2
Leon Chou <bleepbloopsify@gmail.com>
2016-12-08 23:50:23 -0500

    creating custom positions

--- lib/analysis.py
+++ lib/analysis.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
 import pygame as game
-import numpy as np
+from pykinect2 import PyKinectV2
+import ctypes
 
 import logging
 import math
 import csv
+
+import kinectwrapper as kinect
 
 """Analyzes stored file streams for analysis surface"""
 
@@ -22,7 +25,7 @@ _LOGGER = logging.getLogger('analysis')
 class AnalysisStream(object):
 
     def __init__(self, kinect, filename=None):
-        self._kinect = kinect  # KinectStream
+        self._kinect = kinect
         self._analysis_surface = game.Surface(
             (ANALYSIS_WIDTH, self._kinect.colorFrameDesc().Height))
         if filename:
@@ -58,28 +61,39 @@ class AnalysisStream(object):
         outline = [None for i in range(25)]
         if frame:
             mid = surface.get_width() / 2
-            outline[0] = (mid,
-                          surface.get_height() / 4 * 3, 0)
+            outline[0] = PyKinectV2._CameraSpacePoint(
+                body.joints[0].Position.x, body.joints[0].Position.y,
+                body.joints[0].Position.z)
+            # outline[0] = (mid,
+            #               surface.get_height() / 2, 0)
             for count, (start_limb, end_limb) in enumerate(kinect.traverse()):
                 if not outline[start_limb]:
                     continue
                 length = self._kinect.getBoneLength(count)
-                outline[end_limb] = self.get_coords(
-                    outline[start_limb], frame[end_limb], length)
+                outline[end_limb] = PyKinectV2._CameraSpacePoint(
+                    *self.get_coords(outline[start_limb],
+                                     frame[end_limb], length))
+                # outline[end_limb] = self.get_coords(
+                #     outline[start_limb], frame[end_limb], length)
+
+            color_space = [
+                self._kinect._kinect._mapper.MapCameraPointToColorSpace(x)
+                for x in outline]
 
             lines = []
-            for start, end in traverse():
-                if end in D_FLIP:
-                    x1 = self.flip_coord(outline[start][0], mid)
-                    x2 = self.flip_coord(outline[end][0], mid)
-                elif end in FLIP:
-                    x1 = outline[start][0]
-                    x2 = self.flip_coord(outline[end][0], mid)
-                else:
-                    x1 = outline[start][0]
-                    x2 = outline[end][0]
+            for start, end in kinect.traverse():
+                #     if end in D_FLIP:
+                #         x1 = self.flip_coord(outline[start][0], mid)
+                #         x2 = self.flip_coord(outline[end][0], mid)
+                #     elif end in FLIP:
+                #         x1 = outline[start][0]
+                #         x2 = self.flip_coord(outline[end][0], mid)
+                #     else:
+                #         x1 = outline[start][0]
+                #         x2 = outline[end][0]
                 lines.append(
-                    ((x1, outline[start][1]), (x2, outline[end][1])))
+                    ((color_space[start].x, color_space[start].y),
+                     (color_space[end].x, color_space[end].y)))
             return lines
 
     def getSurface(self):
@@ -94,12 +108,12 @@ class AnalysisStream(object):
     def get_coords(self, start, quat, length):
         r = [0, 0, length, 0]
         q_conj = [quat[0], -1 * quat[1], -1 * quat[2], -1 * quat[3]]
-        return [x + y for x, y in zip(self.quaternion_multiply(
-            self.quaternion_multiply(quat, r), q_conj)[1:], start)]
+        end_v = self.quaternion_multiply(
+            self.quaternion_multiply(quat, r), q_conj)[1:]
+        return (end_v[0] + start.x, end_v[1] + start.y, + end_v[2] + start.z)
 
     def prepSurface(self):
         self._analysis_surface.fill((0, 0, 0))
-        # pass
 
     def adjustFramePos(self, position):
         self._curr = position
--- lib/gameinterface.py
+++ lib/gameinterface.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import pygame as game
+import ctypes
 
 import logging
 
@@ -26,6 +27,8 @@ STATE_RECORD = 'RECORD'
 STATE_WAITING = 'WAITING'
 STATE_COMPARE = 'COMPARE'
 
+STATUS_HEIGHT = 50
+
 _LOGGER = logging.getLogger('gameinterface')
 
 FPS = 30
@@ -44,7 +47,7 @@ class GameInterface(object):
         self._analysis = analysis.AnalysisStream(self._kinect, filename)
         self._analysis_width = self._analysis.get_width()
         screen_width = self._infoObject.current_w >> 1
-        screen_height = self._infoObject.current_h >> 1
+        screen_height = (self._infoObject.current_h >> 1) + STATUS_HEIGHT
         if self._state == STATE_COMPARE or self._state == STATE_WAITING:
             screen_width += self._analysis_width
         self._screen = game.display.set_mode((screen_width,
@@ -54,6 +57,8 @@ class GameInterface(object):
                                              game.RESIZABLE, 32)
         _LOGGER.debug('Screen width: {}, Screen height: {}'.format(
             self._screen.get_width(), self._screen.get_height()))
+        self._status_bar = game.Surface(
+            (self._screen.get_width(), STATUS_HEIGHT))
         game.display.set_caption('Fluid Body Analyser')
         self._clock = game.time.Clock()
         self._callback = callback
@@ -96,6 +101,8 @@ class GameInterface(object):
                                    scaled_height))
             self._screen.blit(
                 draw_analysis, (draw_surface.get_width(), 0))
+        self._screen.blit(self._status_bar,
+                          (0, self._screen.get_height() - STATUS_HEIGHT))
         draw_surface = draw_analysis = analysis_surface = None
         game.display.update()
         game.display.flip()
@@ -116,58 +123,57 @@ class GameInterface(object):
                 game.draw.line(surface, color, start, end, width)
                 game.draw.circle(surface, color, map(
                     lambda i: int(i), end), 20, 0)
-                jointnum = font.render(str(traversal[index][1]), 0,
-                                       game.color.THECOLORS['black'])
-                surface.blit(jointnum, map(lambda c: c - 20, end))
+                # jointnum = font.render(str(traversal[index][1]), 0,
+                #                        game.color.THECOLORS['black'])
+                # surface.blit(jointnum, map(lambda c: c - 20, end))
             except Exception as e:
                 pass
 
+    def event_trigger(self, event):
+        if event.type == game.QUIT:
+            # stop_listening()
+            self.quit()
+        elif event.type == game.VIDEORESIZE:  # window resized
+            self._screen = game.display.set_mode(
+                event.dict['size'],
+                game.HWSURFACE | game.DOUBLEBUF |
+                game.RESIZABLE, 32)
+        elif event.type == game.KEYDOWN:
+            state_record = self._state == STATE_RECORD
+            state_view = self._state == STATE_VIEW
+            state_waiting = self._state == STATE_WAITING
+            if event.key == game.K_RETURN:
+                if state_record or state_view:
+                    if state_view:
+                        self._state = STATE_RECORD
+                    else:
+                        self._state = STATE_VIEW
+                    if self._state == STATE_RECORD:
+                        self._kinect.initRecord()
+                else:
+                    if state_waiting:
+                        _LOGGER.info('Start comparison')
+                        self._state = STATE_COMPARE
+                    else:
+                        self._state = STATE_WAITING
+            elif event.key == game.K_p:
+                if self._pause:
+                    _LOGGER.info('Unpause')
+                else:
+                    _LOGGER.info('Pause')
+                self._pause = not self._pause
+            elif event.key == game.K_m:
+                self._audio.mute()
+
     def run(self):
-        screen, kinect = self._screen, self._kinect,
+        screen, kinect = self._screen, self._kinect
         surface, analysis = self._surface, self._analysis
 
         # stop_listening = self._audio.listen()
         while True:
             for event in game.event.get():
-                if event.type == game.QUIT:
-                    # stop_listening()
-                    self.quit()
-                elif event.type == game.VIDEORESIZE:  # window resized
-                    self._screen = game.display.set_mode(
-                        event.dict['size'],
-                        game.HWSURFACE | game.DOUBLEBUF |
-                        game.RESIZABLE, 32)
-                elif event.type == game.KEYDOWN:
-                    if event.key == game.K_RETURN:
-                        state_record = self._state == STATE_RECORD
-                        state_view = self._state == STATE_VIEW
-                        if state_record or state_view:
-                            self._state = STATE_RECORD
-                            if self._state == STATE_VIEW:
-                                self._state = STATE_RECORD
-                            else:
-                                self._state = STATE_VIEW
-                            if self._state == STATE_RECORD:
-                                kinect.initRecord()
-                        else:
-                            if self._state == STATE_WAITING:
-                                self._state = STATE_COMPARE
-                            else:
-                                self._state = STATE_WAITING
-                    elif event.key == game.K_SPACE:
-                        if kinect:
-                            for body in self._bodies:
-                                if body.is_tracked:
-                                    _LOGGER.info('Initializing body lengths')
-                                    kinect.initialize(body)
-                    elif event.key == game.K_p:
-                        if self._pause:
-                            _LOGGER.info('Unpause')
-                        else:
-                            _LOGGER.info('Pause')
-                        self._pause = not self._pause
-                    elif event.key == game.K_m:
-                        self._audio.mute()
+                self.event_trigger(event)
+
             if self._pause:
                 continue
 
@@ -184,14 +190,14 @@ class GameInterface(object):
                 if not body.is_tracked:
                     continue
                 self.drawLines(kinect.drawBody(body),
-                               self._surface, GAME_COLORS[count])
+                               self._surface, GAME_COLORS[5])
                 if self._state == STATE_RECORD:
                     kinect.recordFrame(body)
                 elif self._state == STATE_COMPARE:
                     analysis.prepSurface()
                     self.drawLines(
                         analysis.getBody(body),
-                        analysis._analysis_surface, GAME_COLORS[count])
+                        analysis._analysis_surface, GAME_COLORS[0])
                 elif self._state == STATE_WAITING:
                     analysis.prepSurface()
 
